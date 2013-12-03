78d7d810971b26d9f14a4283937890b2c7e0d8c9
Nex <nex@cuckoobox.org>
2013-12-03 15:40:49 +0100

    Updated output functions

--- viper/common/out.py
+++ viper/common/out.py
@@ -6,16 +6,16 @@ def print_info(message):
     print(bold(cyan("[*]")) + " {0}".format(message))
 
 def print_item(message, tabs=0):
-    print("{0}".format("\t" * tabs) + cyan("*") + " {0}".format(message))
+    print("{0}".format("  " * tabs) + cyan("*") + " {0}".format(message))
 
 def print_warning(message):
-    print(bold(yellow("[!]")) + " WARNING: {0}".format(message))
+    print(bold(yellow("[!]")) + " {0}".format(message))
 
 def print_error(message):
-    print(bold(red("[!]")) + " ERROR: {0}".format(message))
+    print(bold(red("[!]")) + " {0}".format(message))
 
 def print_success(message):
-    print(bold(green("[+]")) + " DONE: {0}".format(message))
+    print(bold(green("[+]")) + " {0}".format(message))
 
 def table(header, rows):
     table = PrettyTable(header)
