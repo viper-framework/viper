f2774fa295fc9fea1f44de5f8b2988276536ebda
Nex <nex@cuckoobox.org>
2014-02-05 21:53:05 +0100

    Added scan for common PE resources

--- modules/pe.py
+++ modules/pe.py
@@ -1,4 +1,5 @@
 import getopt
+import hashlib
 
 try:
     import pefile
@@ -42,6 +43,11 @@ class PE(Module):
 
         return file_type
 
+    def __get_md5(self, data):
+        md5 = hashlib.md5()
+        md5.update(data)
+        return md5.hexdigest()
+
     def imports(self):
         if not self.pe:
             return
@@ -80,13 +86,14 @@ class PE(Module):
             print("")
 
         try:
-            opts, argv = getopt.getopt(self.args[1:], 'hd:', ['help', 'dump='])
+            opts, argv = getopt.getopt(self.args[1:], 'hd:s', ['help', 'dump=', 'scan'])
         except getopt.GetoptError as e:
             print(e)
             usage()
             return
 
         dump_to = None
+        do_scan = False
 
         for opt, value in opts:
             if opt in ('-h', '--help'):
@@ -94,52 +101,90 @@ class PE(Module):
                 return
             elif opt in ('-d', '--dump'):
                 dump_to = value
+            elif opt in ('-s', '--scan'):
+                do_scan = True
 
-        resources = []
+        def get_resources(pe):
+            resources = []
+            if hasattr(pe, 'DIRECTORY_ENTRY_RESOURCE'):
+                for resource_type in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+                    try:
+                        resource = {}
 
-        if hasattr(self.pe, 'DIRECTORY_ENTRY_RESOURCE'):
-            for resource_type in self.pe.DIRECTORY_ENTRY_RESOURCE.entries:
-                try:
-                    resource = {}
+                        if resource_type.name is not None:
+                            name = str(resource_type.name)
+                        else:
+                            name = str(pefile.RESOURCE_TYPE.get(resource_type.struct.Id))
 
-                    if resource_type.name is not None:
-                        name = str(resource_type.name)
-                    else:
-                        name = str(pefile.RESOURCE_TYPE.get(resource_type.struct.Id))
+                        if name == None:
+                            name = str(resource_type.struct.Id)
 
-                    if name == None:
-                        name = str(resource_type.struct.Id)
+                        if hasattr(resource_type, 'directory'):
+                            for resource_id in resource_type.directory.entries:
+                                if hasattr(resource_id, 'directory'):
+                                    for resource_lang in resource_id.directory.entries:
+                                        data = pe.get_data(resource_lang.data.struct.OffsetToData, resource_lang.data.struct.Size)
+                                        filetype = self.__get_filetype(data)
+                                        md5 = self.__get_md5(data)
+                                        language = pefile.LANG.get(resource_lang.data.lang, None)
+                                        sublanguage = pefile.get_sublang_name_for_lang(resource_lang.data.lang, resource_lang.data.sublang)
+                                        offset = ('%-8s' % hex(resource_lang.data.struct.OffsetToData)).strip()
+                                        size = ('%-8s' % hex(resource_lang.data.struct.Size)).strip()
 
-                    if hasattr(resource_type, 'directory'):
-                        for resource_id in resource_type.directory.entries:
-                            if hasattr(resource_id, 'directory'):
-                                for resource_lang in resource_id.directory.entries:
-                                    data = self.pe.get_data(resource_lang.data.struct.OffsetToData, resource_lang.data.struct.Size)
-                                    filetype = self.__get_filetype(data)
-                                    language = pefile.LANG.get(resource_lang.data.lang, None)
-                                    sublanguage = pefile.get_sublang_name_for_lang(resource_lang.data.lang, resource_lang.data.sublang)
-                                    offset = ('%-8s' % hex(resource_lang.data.struct.OffsetToData)).strip()
-                                    size = ('%-8s' % hex(resource_lang.data.struct.Size)).strip()
+                                        resource = [name, offset, md5, size, filetype, language, sublanguage]
 
-                                    resource = [name, offset, size, filetype, language, sublanguage]
+                                        # Dump resources if requested to and if the file currently being
+                                        # processed is the opened session file.
+                                        if dump_to and pe == self.pe:
+                                            resource_path = os.path.join(dump_to, '{0}_{1}_{2}'.format(__session__.file.md5, offset, name))
+                                            resource.append(resource_path)
 
-                                    if dump_to:
-                                        resource_path = os.path.join(dump_to, '{0}_{1}_{2}'.format(__session__.file.md5, offset, name))
-                                        resource.append(resource_path)
+                                            with open(resource_path, 'wb') as resource_handle:
+                                                resource_handle.write(data)
 
-                                        with open(resource_path, 'wb') as resource_handle:
-                                            resource_handle.write(data)
+                                        resources.append(resource)
+                    except Exception as e:
+                        print_error(e)
+                        continue
+            
+            return resources
 
-                                    resources.append(resource)
-                except Exception as e:
-                    print_error(e)
-                    continue
+        resources = get_resources(self.pe)
 
-        headers = ['Name', 'Offset', 'Size', 'File Type', 'Language', 'Sublanguage']
+        headers = ['Name', 'Offset', 'MD5', 'Size', 'File Type', 'Language', 'Sublanguage']
         if dump_to:
             headers.append('Dumped To')
 
         print table(headers, resources)
+
+        if do_scan:
+            print_info("Scanning the repository for matching samples...")
+
+            db = Database()
+            samples = db.find(key='all')
+
+            matches = []
+            for sample in samples:
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try:
+                    cur_pe = pefile.PE(sample_path)
+                except:
+                    continue
+
+                cur_resources = get_resources(cur_pe)
+
+                for cur_resource in cur_resources:
+                    for resource in resources:
+                        if cur_resource[2] == resource[2]:
+                            matches.append([sample.name, sample.sha256, resource[2]])
+
+            print_info("{0} relevant matches found".format(bold(len(matches))))
+
+            if len(matches) > 0:
+                print(table(header=['Name', 'SHA256', 'Resource MD5'], rows=matches))
 
     def imphash(self):
         if not self.pe:
@@ -202,7 +247,9 @@ class PE(Module):
                     matches.append([sample.name, sample.sha256])
 
             print_info("{0} relevant matches found".format(bold(len(matches))))
-            print(table(header=['Name', 'SHA256'], rows=matches))
+
+            if len(matches) > 0:
+                print(table(header=['Name', 'SHA256'], rows=matches))
 
     def usage(self):
         print("usage: pe <command>")
