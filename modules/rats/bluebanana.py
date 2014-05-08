3bfd7b549841a01f8e62d9ea30b0d90776241def
Nex <nex@cuckoobox.org>
2014-05-08 10:43:03 +0200

    Fixed naming and code style of cybergate and bluebanana modules

--- modules/rats/bluebanana.py
+++ modules/rats/bluebanana.py
@@ -8,40 +8,44 @@ from zipfile import ZipFile
 from cStringIO import StringIO
 from Crypto.Cipher import AES
 
-
 from viper.common.out import *
 
-def DecryptAES(enckey, data):
-	cipher = AES.new(enckey) # set the cipher
-	return cipher.decrypt(data) # decrpyt the data
+def decrypt_aes(key, data):
+    cipher = AES.new(key)
+    return cipher.decrypt(data)
 
-def decryptConf(conFile):
-	key1 = "15af8sd4s1c5s511"
-	key2 = "4e3f5a4c592b243f"
-	first = DecryptAES(key1, conFile.decode('hex'))
-	second = DecryptAES(key2, first[:-16].decode('hex'))
-	return second
-	
-def configParse(confRaw):
-	conf = {}
-	clean = filter(lambda x: x in string.printable, confRaw)
-	list = clean.split("<separator>")
-	conf["Domain"] = list[0]
-	conf["Password"] = list[1]
-	conf["Port1"] = list[2]
-	conf["Port2"] = list[3]
-	if len(list) > 4:
-		conf["InstallName"] = list[4]
-		conf["JarName"] = list[5]
-	return conf
+def decrypt_conf(conFile):
+    key1 = '15af8sd4s1c5s511'
+    key2 = '4e3f5a4c592b243f'
+    first = decrypt_aes(key1, conFile.decode('hex'))
+    second = decrypt_aes(key2, first[:-16].decode('hex'))
+    return second
+    
+def extract_config(raw_conf):
+    conf = {}
+    clean = filter(lambda x: x in string.printable, raw_conf)
+    fields = clean.split('<separator>')
+
+    conf['Domain'] = fields[0]
+    conf['Password'] = fields[1]
+    conf['Port1'] = fields[2]
+    conf['Port2'] = fields[3]
+
+    if len(fields) > 4:
+        conf['InstallName'] = fields[4]
+        conf['JarName'] = fields[5]
+
+    return conf
 
 def config(data):
-	newZip = StringIO(data)
-	with ZipFile(newZip) as zip:
-		for name in zip.namelist(): # get all the file names
-			if name == "config.txt": # this file contains the encrypted config
-				conFile = zip.read(name)
-	if conFile: # 
-		confRaw = decryptConf(conFile)
-		conf = configParse(confRaw)
-	return conf
+    new_zip = StringIO(data)
+    with ZipFile(new_zip) as zip_handle:
+        for name in zip_handle.namelist():
+            # This file contains the encrypted config.
+            if name == 'config.txt':
+                conf_data = zip_handle.read(name)
+    if conf_data:
+        raw_conf = decrypt_conf(conf_data)
+        conf = extract_config(raw_conf)
+
+    return conf
