904a214941d9a92cae7a0bc16992f59ce3f964bc
Nex <nex@cuckoobox.org>
2014-05-08 11:13:38 +0200

    Made pdf.py a generic module preparing for future support of more features

--- modules/pdf.py
+++ modules/pdf.py
@@ -1,12 +1,6 @@
 # This file is part of Viper - https://github.com/botherder/viper
 # See the file 'LICENSE' for copying permission.
 
-'''
-This is just a wrapper for Didier Stevens PDF Tools
-http://blog.didierstevens.com/programs/pdf-tools/
-'''
-
-
 import os
 import getopt
 import json
@@ -17,51 +11,71 @@ from viper.core.session import __session__
 
 from pdftools.pdfid import *
 
-
-class PDFID(Module):
-    cmd = 'pdfid'
+class PDF(Module):
+    cmd = 'pdf'
     description = 'Extract PDF Stream Information'
-    authors = ['Kevin Breen']
+    authors = ['Kevin Breen', 'nex']
+
+    def pdf_id(self):
+        if not __session__.is_set():
+            print_error('No session opened')
+            return
+
+        # Run the parser - Returns an XML DOM Instance.
+        pdf_data = PDFiD(__session__.file.path, False, True)
+
+        # This converts to string.
+        #pdf_string = PDFiD2String(pdf_data, True)
+
+        # This converts to JSON.
+        pdf_json = PDFiD2JSON(pdf_data, True)
+
+        # Convert from string.
+        pdf = json.loads(pdf_json)[0]
+
+        # Get general info and format.
+        info = [
+            ['PDF Header', pdf['pdfid']['header']],
+            ['Total Entropy', pdf['pdfid']['totalEntropy']],
+            ['Entropy In Streams', pdf['pdfid']['streamEntropy']],
+            ['Entropy Out Streams', pdf['pdfid']['nonStreamEntropy']],
+            ['Count %% EOF', pdf['pdfid']['countEof']],
+            ['Data After EOF', pdf['pdfid']['countChatAfterLastEof']]
+        ]
+        
+        # If there are date sections lets get them as well.
+        dates = pdf['pdfid']['dates']['date']
+        for date in dates:
+            info.append([date['name'],date['value']])
+
+        # Get streams, counts and format.
+        streams = []
+        for stream in pdf['pdfid']['keywords']['keyword']:
+            streams.append([stream['name'], stream['count']])
+        
+        print_info("General Info")
+        print(table(header=['Desc','Value'], rows=info))
+
+        print_info("Streams & Count")
+        print(table(header=['Name','Count'], rows=streams))
+
+    def usage(self):
+        print("usage: pdf <command>")
+
+    def help(self):
+        self.usage()
+        print("")
+        print("Options:")
+        print("\thelp\t\tShow this help message")
+        print("\tid\t\tShow general information on the PDF")
+        print("")
 
     def run(self):
-        if not __session__.is_set():
-            print_error("No session opened")
+        if len(self.args) == 0:
+            self.help()
             return
-        # Run the parser - Returns an XML DOM Instance
-        print_info('Parsing PDF')
-        pdfdata = PDFiD(__session__.file.path, False, True)
-        # This converts to string
-        pdfstring = PDFiD2String(pdfdata, True)
-        # this converts to JSON
-        pdfJSON = PDFiD2JSON(pdfdata, True)
-        # convert from string
-        this = json.loads(pdfJSON)
-        jsonString = this[0]
-        # Get general info and format
-        genRows = []
-        genRows.append(['PDF Header', jsonString["pdfid"]["header"]])
-        genRows.append(['Total Entropy', jsonString["pdfid"]["totalEntropy"]])
-        genRows.append(['Entropy In Streams', jsonString["pdfid"]["streamEntropy"]])
-        genRows.append(['Entropy Out Streams', jsonString["pdfid"]["nonStreamEntropy"]])
-        genRows.append(['Count %% EOF', jsonString["pdfid"]["countEof"]])
-        genRows.append(['Data After EOF', jsonString["pdfid"]["countChatAfterLastEof"]])
-        
-        # If there are date sections lets get them as well
 
-        dates = jsonString["pdfid"]["dates"]["date"]
-        for date in dates:
-            genRows.append([date['name'],date['value']])
-
-        # get streams and counts then format
-        streamRows = []
-        pdfStreams = jsonString["pdfid"]["keywords"]["keyword"]
-        for stream in pdfStreams:
-            streamRows.append([stream["name"], stream["count"]])
-        
-        # Print the tables
-        print_info("General Info")
-        print(table(header=['Desc','Value'], rows=genRows))
-
-        print_info("Stream & Count")
-        print(table(header=['Name','Count'], rows=streamRows))
-
+        if self.args[0] == 'help':
+            self.help()
+        elif self.args[0] == 'id':
+            self.pdf_id()
