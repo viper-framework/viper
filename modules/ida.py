8b1108acb518f92c5c20b807ad4d549fda2ebf48
Raphaël Vinot <raphael@vinot.info>
2014-07-17 10:37:07 +0200

    Cleanup, use Popen, add linux and win support

--- modules/ida.py
+++ modules/ida.py
@@ -1,76 +1,82 @@
+# -*- coding: utf-8 -*-
 # This file is part of Viper - https://github.com/botherder/viper
 # See the file 'LICENSE' for copying permission.
 
 import os
-import getopt
+import shlex
+import subprocess
 import magic
-import pefile
+import sys
 
-from viper.common.out import *
+from viper.common.out import print_error
 from viper.common.abstracts import Module
 from viper.core.session import __sessions__
 
-IMAGE_DLL_CHARACTERISTICS_DRIVER  = 0x400
 ext = ".bin"
+
+run_ida = {'linux2': 'idaq', 'darwin': 'open -a idaq',
+           'win32': 'idaq'}
+
 
 class Ida(Module):
     cmd = 'ida'
     description = 'Start IDA Pro'
-    authors = ['Sascha Rommelfangen']
+    authors = ['Sascha Rommelfangen', 'Raphaël Vinot']
+    is_64b = False
+    ext = ''
 
-    def OpenIDA(self, filename, ext, arch):
+    def OpenIDA(self, filename):
         directory = filename + ".dir"
         if not os.path.exists(directory):
             os.makedirs(directory)
-        destination = directory + "/executable" + ext
+        destination = directory + "/executable" + self.ext
         print destination
         if not os.path.lexists(destination):
             os.link(filename, destination)
-        if arch == 0:
-            command = "open -a idaq " + destination
-            os.system(command)
-        if arch == 1:
-            command = "open -a idaq64 " + destination
-            os.system(command)
+        if self.is_64b:
+            command_line = '{}64 {}'.format(run_ida[sys.platform], destination)
+        else:
+            command_line = '{} {}'.format(run_ida[sys.platform], destination)
+        args = shlex.split(command_line)
+        subprocess.Popen(args)
 
     def run(self):
         if not __sessions__.is_set():
             print_error("No session opened")
             return
 
-        try:
-            filename = __sessions__.current.file.path
-            type = magic.from_file(filename)
-            if "PE32 " in type:
-                if "DLL" in type:
-                    print "32 bit DLL"
-                    if "native" in type:
-                        print "perhaps a driver (.sys)" 
-                    ext = ".dll"
-                else:
-                     print "32 bit EXE"
-                     ext = ".exe"
-                try:
-                    self.OpenIDA(filename, ext, 0)
-                except:
-                    print_error("Unable to start IDA Pro")
-            elif "PE32+" in type:
-                if "DLL" in type:
-                    print "64 bit DLL"
-                    if "native" in type:
-                        print "perhaps a driver (.sys)"
-                    ext = ".dll"
-                else:
-                    print "64 bit EXE"
-                    ext = ".exe"
-                try:
-                    self.OpenIDA(filename, ext, 1)
-                except:
-                    print_error("Unable to start IDA Pro")
-            else:
-                print "not recognized"  
-                print type
-        except OSError:
-            print_error("IDA is not installed")
+        filename = __sessions__.current.file.path
+        filetype = magic.from_file(filename)
+        if 'x86-64' in filetype:
+            self.is_64b = True
+        if 'DLL' in filetype:
+            self.ext = '.dll'
+        elif 'PE32' in filetype:
+            self.ext = '.exe'
+        elif 'shared object' in filetype:
+            self.ext = '.so'
+        elif 'ELF' in filetype:
+            self.ext = ''
+        else:
+            print "not recognized"
+            print type
             return
 
+        arch = '64' if self.is_64b else '32'
+
+        if self.ext == '':
+            print ' '.join([arch, 'bit executable (linux)'])
+        elif self.ext == '.so':
+            print ' '.join([arch, 'bit shared object (linux)'])
+        elif self.ext == '.exe':
+            print ' '.join([arch, 'bit executable (Windows)'])
+        else:
+            to_print = [arch, 'bit DLL (Windows)']
+            if "native" in filetype:
+                to_print.append('perhaps a driver (.sys)')
+            print ' '.join(to_print)
+
+        try:
+            self.OpenIDA(filename)
+        except:
+            print_error("Unable to start IDA Pro")
