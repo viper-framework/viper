baf33f836b26c5e0ac80106b19e75747c351ddf8
Nex <nex@nex.sx>
2014-07-18 12:37:27 +0200

    Fixed things around in ida module

--- modules/ida.py
+++ modules/ida.py
@@ -3,12 +3,11 @@
 # See the file 'LICENSE' for copying permission.
 
 import os
+import sys
 import shlex
 import subprocess
-import magic
-import sys
 
-from viper.common.out import print_error
+from viper.common.out import *
 from viper.common.abstracts import Module
 from viper.core.session import __sessions__
 
@@ -17,26 +16,31 @@ ext = ".bin"
 run_ida = {'linux2': 'idaq', 'darwin': 'open -a idaq',
            'win32': 'idaq'}
 
-
 class Ida(Module):
     cmd = 'ida'
     description = 'Start IDA Pro'
     authors = ['Sascha Rommelfangen', 'Raphaël Vinot']
-    is_64b = False
-    ext = ''
 
-    def OpenIDA(self, filename):
+    def __init__(self):
+        self.is_64b = False
+        self.ext = ''
+
+    def open_ida(self, filename):
         directory = filename + ".dir"
+
         if not os.path.exists(directory):
             os.makedirs(directory)
+
         destination = directory + "/executable" + self.ext
-        print destination
+
         if not os.path.lexists(destination):
             os.link(filename, destination)
+
         if self.is_64b:
             command_line = '{}64 {}'.format(run_ida[sys.platform], destination)
         else:
             command_line = '{} {}'.format(run_ida[sys.platform], destination)
+
         args = shlex.split(command_line)
         subprocess.Popen(args)
 
@@ -45,8 +49,7 @@ class Ida(Module):
             print_error("No session opened")
             return
 
-        filename = __sessions__.current.file.path
-        filetype = magic.from_file(filename)
+        filetype = __sessions__.current.file.type
         if 'x86-64' in filetype:
             self.is_64b = True
         if 'DLL' in filetype:
@@ -58,25 +61,25 @@ class Ida(Module):
         elif 'ELF' in filetype:
             self.ext = ''
         else:
-            print "not recognized"
-            print type
+            print_error("File type not supported")
             return
 
         arch = '64' if self.is_64b else '32'
 
         if self.ext == '':
-            print ' '.join([arch, 'bit executable (linux)'])
+            print_info(' '.join([arch, 'bit executable (linux)']))
         elif self.ext == '.so':
-            print ' '.join([arch, 'bit shared object (linux)'])
+            print_info(' '.join([arch, 'bit shared object (linux)']))
         elif self.ext == '.exe':
-            print ' '.join([arch, 'bit executable (Windows)'])
+            print_info(' '.join([arch, 'bit executable (Windows)']))
         else:
             to_print = [arch, 'bit DLL (Windows)']
             if "native" in filetype:
                 to_print.append('perhaps a driver (.sys)')
-            print ' '.join(to_print)
+            
+            print_info(' '.join(to_print))
 
         try:
-            self.OpenIDA(filename)
+            self.open_ida(__sessions__.current.file.path)
         except:
             print_error("Unable to start IDA Pro")
