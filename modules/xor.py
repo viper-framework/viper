24b35a2368b43ba3dd708e2ee26424858a4f5795
Nex <nex@nex.sx>
2014-07-22 00:08:18 +0200

    Small fixes in XOR module

--- modules/xor.py
+++ modules/xor.py
@@ -41,7 +41,7 @@ class XorSearch(Module):
         ]
 
         def usage():
-            print("usage: xor [-x -r -s=term]")
+            print("usage: xor [-x] [-r] [-a] [-s=term]")
 
         def help():
             usage()
@@ -49,8 +49,9 @@ class XorSearch(Module):
             print("Options:")
             print("\t--help (-h)\tShow this help message")
             print("\t--search (-s)\tSpecify a custom term to search")
-            print("\t--xor (-x)\t Search XOR (Default)")
-            print("\t--rot (-r)\t Search ROT")
+            print("\t--xor (-x)\tSearch XOR (default)")
+            print("\t--rot (-r)\tSearch ROT")
+            print("\t--all (-a)\tAttempt search with all available modes")
             print("")
 
         def xordata(data, key):
@@ -60,7 +61,7 @@ class XorSearch(Module):
             return encoded
 
         def rotdata(data, key):
-            coded = ''
+            encoded = ''
             for char in data:
                 if char.isalpha():
                     num = ord(char)
@@ -69,8 +70,8 @@ class XorSearch(Module):
                         num += 26
                     elif num == ord('a') or num == ord('A'):
                         num -= 26
-                    coded += chr(num)
-            return coded.lower()
+                    encoded += chr(num)
+            return encoded.lower()
         
         def xor_search(terms):
             for key in range(1, 256):
@@ -86,13 +87,10 @@ class XorSearch(Module):
                     if term.lower() in roted:
                         print_error("Matched: {0} with ROT: {1}".format(term, key))
                         
-        # Main starts here
-        # Check for open session
         if not __sessions__.is_set():
             print_error("No session opened")
             return
             
-        # get opt and args
         try:
             opts, argv = getopt.getopt(self.args, 'hxras:', ['help', 'xor', 'rot', 'all',  'search='])
         except getopt.GetoptError as e:
@@ -126,7 +124,7 @@ class XorSearch(Module):
         if xor:
             print_info("Searching XOR")
             xor_search(terms)
+
         if rot:
             print_info("Searching ROT")
             rot_search(terms)
-        
