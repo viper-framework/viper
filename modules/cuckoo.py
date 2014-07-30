af6cac4415ab1e933a9f2bb7b95fb9836370b2b6
lnmyshkin <john.nemitz+gh@gmail.com>
2014-07-30 17:11:53 -0500

    Update cuckoo.py

--- modules/cuckoo.py
+++ modules/cuckoo.py
@@ -28,7 +28,7 @@ class Cuckoo(Module):
             return
 
         def usage():
-            print("usage: cuckoo [-H=host] [-p=port]")
+            print("usage: cuckoo [-H=host] [-p=port] [-m=machine]")
 
         def help():
             usage()
@@ -37,10 +37,11 @@ class Cuckoo(Module):
             print("\t--help (-h)\tShow this help message")
             print("\t--host (-H)\tSpecify an host (default: localhost)")
             print("\t--port (-p)\tSpecify a port (default: 8090")
+            print("\t--machine (-m)\tSpecify a machine (default: unspecified)")
             print("")
 
         try:
-            opts, argv = getopt.getopt(self.args, 'hH:p:', ['help', 'host=', 'port='])
+            opts, argv = getopt.getopt(self.args, 'hH:p:m:', ['help', 'host=', 'port=', 'machine='])
         except getopt.GetoptError as e:
             print(e)
             usage()
@@ -48,6 +49,7 @@ class Cuckoo(Module):
 
         host = 'localhost'
         port = 8090
+        machine = None
 
         for opt, value in opts:
             if opt in ('-h', '--help'):
@@ -59,13 +61,19 @@ class Cuckoo(Module):
             elif opt in ('-p', '--port'):
                 if value:
                     port = value
+            elif opt in ('-m', '--machine'):
+                if value:
+                    machine = value
 
         url = 'http://{0}:{1}/tasks/create/file'.format(host, port)
 
         files = dict(file=open(__sessions__.current.file.path, 'rb'))
 
+        if machine:
+            data = {"machine":machine}
+
         try:
-            response = requests.post(url, files=files)
+            response = requests.post(url, files=files, data=data)
         except requests.ConnectionError:
             print_error("Unable to connect to Cuckoo API at {0}:{1}".format(host, port))
             return
