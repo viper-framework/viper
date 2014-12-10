0ef6e1bdbd8a5a805779d44a19c7fe5664712ece
Raphaël Vinot <raphael@vinot.info>
2014-12-10 14:48:26 +0100

    Use argparse in the editdistance module, make pep8 happy

--- modules/editdistance.py
+++ modules/editdistance.py
@@ -3,11 +3,9 @@
 
 import itertools
 
-from viper.common.out import *
 from viper.common.abstracts import Module
 from viper.core.session import __sessions__
 from viper.core.database import Database
-from viper.core.storage import get_sample_path
 
 
 class Editdistance(Module):
@@ -15,16 +13,8 @@ class Editdistance(Module):
     description = 'Edit distance on the filenames'
     authors = ['emdel', 'nex']
 
-
-    def usage(self):
-        self.log('', "Usage: editdistance")
-
-    def help(self):
-        self.usage()
-        self.log('', "")
-        self.log('', "Options:")
-        self.log('', "\thelp\tShow this help message")
-        self.log('', "") 
+    def __init__(self):
+        super(Editdistance, self).__init__()
 
     def edit(self):
         db = Database()
@@ -37,42 +27,41 @@ class Editdistance(Module):
 
             filenames.append(sample.name)
 
-        
         # from http://hetland.org/coding/python/levenshtein.py
         def levenshtein(a, b):
             "Calculates the Levenshtein distance between a and b."
             n, m = len(a), len(b)
             if n > m:
                 # Make sure n <= m, to use O(min(n,m)) space
-                a,b = b,a
-                n,m = m,n
+                a, b = b, a
+                n, m = m, n
 
-            current = range(n+1)
-            for i in range(1,m+1):
-                previous, current = current, [i]+[0]*n
-                for j in range(1,n+1):
-                    add, delete = previous[j]+1, current[j-1]+1
-                    change = previous[j-1]
-                    if a[j-1] != b[i-1]:
+            current = range(n + 1)
+            for i in range(1, m + 1):
+                previous, current = current, [i] + [0] * n
+                for j in range(1, n + 1):
+                    add, delete = previous[j] + 1, current[j - 1] + 1
+                    change = previous[j - 1]
+                    if a[j - 1] != b[i - 1]:
                         change = change + 1
                     current[j] = min(add, delete, change)
 
             return current[n]
 
         distance = []
-        for i in itertools.combinations(filenames, 2): 
+        for i in itertools.combinations(filenames, 2):
             edit = levenshtein(i[0], i[1])
             distance.append(edit)
- 
-        self.log('info', "Average Edit distance: {0}".format(sum(distance)/len(distance))) 
+
+        self.log('info', "Average Edit distance: {0}".format(sum(distance) / len(distance)))
 
     def run(self):
+        super(Editdistance, self).run()
+        if self.parsed_args is None:
+            return
+
         if not __sessions__.is_set():
             self.log('error', "No session opened")
-            return
- 
-        if len(self.args) != 0:
-            self.help()
             return
 
         self.edit()
