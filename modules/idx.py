ef49260929f61afe97f2566a9d690a3e12b87250
Raphaël Vinot <raphael@vinot.info>
2014-12-11 14:55:18 +0100

    make pep8 happy, remove unused variables and imports.

--- modules/idx.py
+++ modules/idx.py
@@ -3,16 +3,13 @@
 # See the file 'LICENSE' for copying permission.
 
 
-import os
-import getopt
 import struct
-import sys
 import time
 import zlib
 
-from viper.common.out import *
 from viper.common.abstracts import Module
 from viper.core.session import __sessions__
+
 
 class Idx(Module):
     cmd = 'idx'
@@ -54,8 +51,6 @@ class Idx(Module):
             data.seek(32)
             len_URL = struct.unpack('b', data.read(1))[0]
             data_URL = data.read(len_URL)
-            namespace_len = struct.unpack('>h', data.read(2))[0]
-            namespace = data.read(namespace_len)
             sec2_fields = struct.unpack('>l', data.read(4))[0]
             sec_two.append(['URL', data_URL])
 
@@ -74,15 +69,15 @@ class Idx(Module):
             Section three contains a copy of the JAR manifest data.
             """
             sec_three = []
-            data.seek (128+sec2_len)
+            data.seek(128 + sec2_len)
             sec3_data = data.read(sec3_len)
 
-            if sec3_data[0:3] == '\x1F\x8B\x08': # Valid GZIP header
-                sec3_unc = zlib.decompress(sec3_data, 15+32) # Trick to force bitwindow size
+            if sec3_data[0:3] == '\x1F\x8B\x08':  # Valid GZIP header
+                sec3_unc = zlib.decompress(sec3_data, 15 + 32)  # Trick to force bitwindow size
                 sec_split = sec3_unc.strip().split('\n')
                 for line in sec_split:
                     k, v = line.split(':')
-                    sec_three.append([k,v.replace('\x0d', '')])
+                    sec_three.append([k, v.replace('\x0d', '')])
             return sec_three
 
         def sec4_parse():
@@ -96,31 +91,33 @@ class Idx(Module):
             # ToDo Export any found data blocks or objects
             sec_four = []
             unknowns = 0
-            data.seek (128 + sec2_len + sec3_len)
+            data.seek(128 + sec2_len + sec3_len)
             sec4_magic, sec4_ver = struct.unpack('>HH', data.read(4))
-            if sec4_magic == 0xACED: # Magic number for Java serialized data, version always appears to be 5
-                while not data.tell() == file_size: # If current offset isn't at end of file yet
+            if sec4_magic == 0xACED:  # Magic number for Java serialized data, version always appears to be 5
+                while not data.tell() == file_size:  # If current offset isn't at end of file yet
                     if unknowns > 5:
                         return sec_four
                     sec4_type = struct.unpack('B', data.read(1))[0]
                     if sec4_type == 0x77:
                         block_len = struct.unpack('b', data.read(1))[0]
                         sec_four.append("Found Data Block of length {0}".format(block_len))
-                    elif sec4_type == 0x73: #Object
+                    elif sec4_type == 0x73:  # Object
                         sec_four.append("Found Data Object")
                         continue
-                    elif sec4_type == 0x72: #Class Description
+                    elif sec4_type == 0x72:  # Class Description
                         block_len = struct.unpack('>h', data.read(2))[0]
                         sec_four.append("Found Class Description of lenght {0}".format(block_len))
                     else:
                         unknowns += 1
             return sec_four
 
+        if not __sessions__.is_set():
+            self.log('error', "No session opened")
+            return
+
         # Main starts here
         data = open(__sessions__.current.file.path)
         file_size = __sessions__.current.file.size
-        busy_byte = data.read(1)
-        complete_byte = data.read(1)
         cache_ver = struct.unpack('>i', data.read(4))[0]
         if cache_ver not in (602, 603, 604, 605, 606):
             self.log('error', "Invalid IDX header found")
@@ -132,11 +129,10 @@ class Idx(Module):
                 data.seek(8)
             elif cache_ver == 605:
                 data.seek(6)
-            is_shortcut_img = data.read(1)
             content_len = struct.unpack('>l', data.read(4))[0]
-            last_modified_date = struct.unpack('>q', data.read(8))[0]/1000
-            expiration_date = struct.unpack('>q', data.read(8))[0]/1000
-            validation_date = struct.unpack('>q', data.read(8))[0]/1000
+            last_modified_date = struct.unpack('>q', data.read(8))[0] / 1000
+            expiration_date = struct.unpack('>q', data.read(8))[0] / 1000
+            validation_date = struct.unpack('>q', data.read(8))[0] / 1000
 
             sec_one = []
             sec_one.append(['Content Length', content_len])
@@ -152,16 +148,13 @@ class Idx(Module):
                 sec4_len = 0
                 sec5_len = 0
             elif cache_ver in [603, 604, 605]:
-                known_to_be_signed = data.read(1)
                 sec2_len = struct.unpack('>i', data.read(4))[0]
                 sec3_len = struct.unpack('>i', data.read(4))[0]
                 sec4_len = struct.unpack('>i', data.read(4))[0]
                 sec5_len = struct.unpack('>i', data.read(4))[0]
 
-                blacklist_timestamp = struct.unpack('>q', data.read(8))[0]/1000
-                cert_expiration_date = struct.unpack('>q', data.read(8))[0]/1000
-                class_verification_status = data.read(1)
-                reduced_manifest_length = struct.unpack('>l', data.read(4))[0]
+                blacklist_timestamp = struct.unpack('>q', data.read(8))[0] / 1000
+                cert_expiration_date = struct.unpack('>q', data.read(8))[0] / 1000
 
                 sec_one.append(['Section 2 length', sec2_len])
                 if sec3_len:
