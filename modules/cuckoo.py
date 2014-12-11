bf04a5ee3f5954d7de4f45b4fcea06b7f21cc29c
Raphaël Vinot <raphael@vinot.info>
2014-12-11 10:23:26 +0100

    Use argparse in the cuckoo module.

--- modules/cuckoo.py
+++ modules/cuckoo.py
@@ -1,24 +1,31 @@
 # This file is part of Viper - https://github.com/botherder/viper
 # See the file 'LICENSE' for copying permission.
 
-import getopt
-
 try:
     import requests
     HAVE_REQUESTS = True
 except ImportError:
     HAVE_REQUESTS = False
 
-from viper.common.out import *
 from viper.common.abstracts import Module
 from viper.core.session import __sessions__
+
 
 class Cuckoo(Module):
     cmd = 'cuckoo'
     description = 'Submit the file to Cuckoo Sandbox'
     authors = ['nex']
 
+    def __init__(self):
+        super(Cuckoo, self).__init__()
+        self.parser.add_argument('-H', '--host', default='localhost', help='Specify an host. Default: localhost')
+        self.parser.add_argument('-p', '--port', default=8090, type=int, help='Specify an port. Default: 8090')
+
     def run(self):
+        super(Cuckoo, self).run()
+        if self.parsed_args is None:
+            return
+
         if not __sessions__.is_set():
             self.log('error', "No session opened")
             return
@@ -27,38 +34,8 @@ class Cuckoo(Module):
             self.log('error', "Missing dependency, install requests (`pip install requests`)")
             return
 
-        def usage():
-            self.log('', "usage: cuckoo [-H=host] [-p=port]")
-
-        def help():
-            usage()
-            self.log('', "")
-            self.log('', "Options:")
-            self.log('', "\t--help (-h)\tShow this help message")
-            self.log('', "\t--host (-H)\tSpecify an host (default: localhost)")
-            self.log('', "\t--port (-p)\tSpecify a port (default: 8090")
-            self.log('', "")
-
-        try:
-            opts, argv = getopt.getopt(self.args, 'hH:p:', ['help', 'host=', 'port='])
-        except getopt.GetoptError as e:
-            self.log('', e)
-            usage()
-            return
-
-        host = 'localhost'
-        port = 8090
-
-        for opt, value in opts:
-            if opt in ('-h', '--help'):
-                help()
-                return
-            elif opt in ('-H', '--host'):
-                if value:
-                    host = value
-            elif opt in ('-p', '--port'):
-                if value:
-                    port = value
+        host = self.parsed_args.host
+        port = self.parsed_args.port
 
         url = 'http://{0}:{1}/tasks/create/file'.format(host, port)
 
@@ -69,5 +46,3 @@ class Cuckoo(Module):
         except requests.ConnectionError:
             self.log('error', "Unable to connect to Cuckoo API at {0}:{1}".format(host, port))
             return
-
-        print response.text
