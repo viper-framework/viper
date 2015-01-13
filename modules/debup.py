aa76798af20029e83ddbd1f6b7f558ee594db127
kevthehermit <kevthehermit@gmail.com>
2015-01-13 15:37:20 -0500

    Update and Fix Debup Module

--- modules/debup.py
+++ modules/debup.py
@@ -7,7 +7,7 @@ from viper.common.abstracts import Module
 from viper.core.session import __sessions__
 
 try:
-    import OleFileIO_PL
+    import olefile
     HAVE_OLE = True
 except ImportError:
     HAVE_OLE = False
@@ -20,8 +20,14 @@ class Debup(Module):
 
     def __init__(self):
         super(Debup, self).__init__()
-        self.parser.add_argument('-s', '--session', action='store_true', help='Switch session to the quarantined file')
-
+        self.parser.add_argument('-s', '--session', action='store_true', default=False, help='Switch session to the quarantined file')
+    
+    def xordata(self, data, key):
+        encoded = bytearray(data)
+        for i in range(len(encoded)):
+            encoded[i] ^= key
+        return encoded
+    
     def run(self):
 
         super(Debup, self).run()
@@ -33,67 +39,55 @@ class Debup(Module):
             return
 
         if not HAVE_OLE:
-            self.log('error', "Missing dependency, install OleFileIO (`pip install OleFileIO_PL`)")
+            self.log('error', "Missing dependency, install olefile (`pip install olefile`)")
             return
-
-        def xordata(data, key):
-            encoded = bytearray(data)
-            for i in range(len(encoded)):
-                encoded[i] ^= key
-            return encoded
-
-        def bupextract():
-            # Check for valid OLE
-            if not OleFileIO_PL.isOleFile(__sessions__.current.file.path):
-                self.log('error', "Not a valid BUP File")
-                return
-            ole = OleFileIO_PL.OleFileIO(__sessions__.current.file.path)
-            # We know that BUPS are xor'd with 6A which is dec 106 for the decoder
-            self.log('info', "Switching Session to Embedded File")
-            data = xordata(ole.openstream('File_0').read(), 106)
-            # this is a lot of work jsut to get a filename.
-            data2 = xordata(ole.openstream('Details').read(), 106)
-            ole.close()
-            lines = data2.split('\n')
-            for line in lines:
-                if line.startswith('OriginalName'):
-                    fullpath = line.split('=')[1]
-                    pathsplit = fullpath.split('\\')
-                    filename = str(pathsplit[-1][:-1])
-            # now lets write the data out to a file and get a session on it
-            if data:
-                tempName = os.path.join('/tmp', filename)
-                with open(tempName, 'w') as temp:
-                    temp.write(data)
-                __sessions__.new(tempName)
-                return
-            else:
-                self.log('error', "Unble to Switch Session")
-
-        # Run Functions
-        if self.parsed_args.session:
-            bupextract()
-
+            
         # Check for valid OLE
-        if not OleFileIO_PL.isOleFile(__sessions__.current.file.path):
+        if not olefile.isOleFile(__sessions__.current.file.path):
             self.log('error', "Not a valid BUP File")
             return
 
-        ole = OleFileIO_PL.OleFileIO(__sessions__.current.file.path)
-        # We know that BUPS are xor'd with 6A which is dec 106 for the decoder
-        details = xordata(ole.openstream('Details').read(), 106)
-        # the rest of this is just formating
-        lines = details.split('\n')
-        rows = []
+        # Extract all the contents from the bup file. 
 
+        ole = olefile.OleFileIO(__sessions__.current.file.path)
+        # We know that BUPS are xor'd with 6A which is dec 106 for the decoder
+        
+        # This is the stored file.
+        data = self.xordata(ole.openstream('File_0').read(), 106)
+        
+        # Get the details page
+        data2 = self.xordata(ole.openstream('Details').read(), 106)
+        
+        # Close the OLE
+        ole.close()
+        
+        # Process the details file
+        rows = []
+        lines = data2.split('\n')
         for line in lines:
+            if line.startswith('OriginalName'):
+                fullpath = line.split('=')[1]
+                pathsplit = fullpath.split('\\')
+                filename = str(pathsplit[-1][:-1])
             try:
                 k, v = line.split('=')
                 rows.append([k, v[:-1]])  # Strip the \r from v
             except:
-                pass
+                pass               
+                
+        # If we opted to switch session then do that
+        if data and self.parsed_args.session:
+            try:
+                tempName = os.path.join('/tmp', filename)
+                with open(tempName, 'w') as temp:
+                    temp.write(data)
+                self.log('info', "Switching Session to Embedded File")
+                __sessions__.new(tempName)
+                return
+            except:
+                self.log('error', "Unble to Switch Session")
+        # Else jsut print the date
+        else:
+            self.log('info', "BUP Details:")
+            self.log('table', dict(header=['Description', 'Value'], rows=rows))           
 
-        self.log('info', "BUP Details:")
-        self.log('table', dict(header=['Description', 'Value'], rows=rows))
-
-        ole.close()
