ff41d8f98ec63a6e36148d5c75fb1799a47cfa20
emanuel.alves <emanuel.alves@anubisnetworks.com>
2015-01-29 10:22:49 +0000

    Fixed slice typos

--- modules/pehash/pehasher.py
+++ modules/pehash/pehasher.py
@@ -38,12 +38,14 @@ def calculate_pehash(file_path=None):
         #image characteristics
         img_chars = bitstring.BitArray(hex(exe.FILE_HEADER.Characteristics))
         #pad to 16 bits
+        """
         img_chars = bitstring.BitArray(bytes=img_chars.tobytes())
         if img_chars.len == 16:
             img_chars_xor = img_chars[0:7] ^ img_chars[8:15]
         else:
             img_chars_xor = img_chars[0:7]
-    
+        """
+        img_chars_xor = img_chars[0:8] ^ img_chars[8:16]
         #start to build pehash
         pehash_bin = bitstring.BitArray(img_chars_xor)
     
@@ -51,7 +53,7 @@ def calculate_pehash(file_path=None):
         sub_chars = bitstring.BitArray(hex(exe.FILE_HEADER.Machine))
         #pad to 16 bits
         sub_chars = bitstring.BitArray(bytes=sub_chars.tobytes())
-        sub_chars_xor = sub_chars[0:7] ^ sub_chars[8:15]
+        sub_chars_xor = sub_chars[0:6] ^ sub_chars[8:16]
         pehash_bin.append(sub_chars_xor)
     
         #Stack Commit Size
@@ -59,7 +61,7 @@ def calculate_pehash(file_path=None):
         stk_size_bits = string.zfill(stk_size.bin, 32)
         #now xor the bits
         stk_size = bitstring.BitArray(bin=stk_size_bits)
-        stk_size_xor = stk_size[8:15] ^ stk_size[16:23] ^ stk_size[24:31]
+        stk_size_xor = stk_size[8:16] ^ stk_size[16:24] ^ stk_size[24:32]
         #pad to 8 bits
         stk_size_xor = bitstring.BitArray(bytes=stk_size_xor.tobytes())
         pehash_bin.append(stk_size_xor)
@@ -69,7 +71,7 @@ def calculate_pehash(file_path=None):
         hp_size_bits = string.zfill(hp_size.bin, 32)
         #now xor the bits
         hp_size = bitstring.BitArray(bin=hp_size_bits)
-        hp_size_xor = hp_size[8:15] ^ hp_size[16:23] ^ hp_size[24:31]
+        hp_size_xor = hp_size[8:16] ^ hp_size[16:24] ^ hp_size[24:32]
         #pad to 8 bits
         hp_size_xor = bitstring.BitArray(bytes=hp_size_xor.tobytes())
         pehash_bin.append(hp_size_xor)
@@ -77,9 +79,9 @@ def calculate_pehash(file_path=None):
         #Section chars
         for section in exe.sections:
             #virutal address
-            sect_va =  bitstring.BitArray(hex(section.VirtualAddress))
-            sect_va = bitstring.BitArray(bytes=sect_va.tobytes())
-            pehash_bin.append(sect_va)    
+            sect_va = bitstring.BitArray(hex(section.VirtualAddress))
+            sect_va = bitstring.BitArray(bytes=sect_va.tobytes())[8:32]
+            pehash_bin.append(sect_va)
     
             #rawsize
             sect_rs =  bitstring.BitArray(hex(section.SizeOfRawData))
@@ -87,13 +89,13 @@ def calculate_pehash(file_path=None):
             sect_rs_bits = string.zfill(sect_rs.bin, 32)
             sect_rs = bitstring.BitArray(bin=sect_rs_bits)
             sect_rs = bitstring.BitArray(bytes=sect_rs.tobytes())
-            sect_rs_bits = sect_rs[8:31]
+            sect_rs_bits = sect_rs[8:32]
             pehash_bin.append(sect_rs_bits)
     
             #section chars
             sect_chars =  bitstring.BitArray(hex(section.Characteristics))
             sect_chars = bitstring.BitArray(bytes=sect_chars.tobytes())
-            sect_chars_xor = sect_chars[16:23] ^ sect_chars[24:31]
+            sect_chars_xor = sect_chars[16:24] ^ sect_chars[24:32]
             pehash_bin.append(sect_chars_xor)
     
             #entropy calulation
@@ -102,14 +104,14 @@ def calculate_pehash(file_path=None):
             raw = exe.write()[address+size:]
             if size == 0: 
                 kolmog = bitstring.BitArray(float=1, length=32)
-                pehash_bin.append(kolmog[0:7])
+                pehash_bin.append(kolmog[0:8])
                 continue
             bz2_raw = bz2.compress(raw)
             bz2_size = len(bz2_raw)
             #k = round(bz2_size / size, 5)
             k = bz2_size / size
             kolmog = bitstring.BitArray(float=k, length=32)
-            pehash_bin.append(kolmog[0:7])
+            pehash_bin.append(kolmog[0:8])
     
         m = hashlib.sha1()
         m.update(pehash_bin.tobytes())
