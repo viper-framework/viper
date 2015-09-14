6fe8035391e5a603244ec87d19bd873a0cb20764
kevthehermit <kevthehermit@gmail.com>
2015-09-14 17:50:04 -0400

    Migrate to MySQL

--- db_migrate.py
+++ db_migrate.py
@@ -54,7 +54,7 @@ def migrate_to_sqlite():
     try:
         shutil.copy('viper.db', 'viper.db.bak')
     except:
-        cont = raw_input("Failed to Backup. Do you wish to continue? Y/n")
+        cont = raw_input("Failed to Backup. Do you wish to continue? Y/n\n")
         if cont != 'Y':
             print "User stopped Migration"
             return
@@ -75,6 +75,46 @@ def migrate_to_sqlite():
         
     print "Migration Complete"
 
+    
+def create_sql(project, db_path, mysql_engine):
+    print "  - Connecting to Viper Database {0}".format(db_path)
+    sqlite_engine = create_engine('sqlite:///{0}'.format(db_path))
+    
+    print "  - Reading data from Sqlite DB"
+    malware = sqlite_engine.execute('SELECT * FROM malware').fetchall()
+    association = sqlite_engine.execute('SELECT * FROM association').fetchall()
+    notes = sqlite_engine.execute('SELECT * FROM note').fetchall()
+    tags = sqlite_engine.execute('SELECT * FROM tag').fetchall()
+    
+    print "  - Adding rows to MySQL Database"
+
+    #add all the rows back in
+    for row in notes:
+        mysql_engine.execute("INSERT INTO {0}_note VALUES ('{1}', '{2}', '{3}')".format(project, row[0], row[1], row[2]))
+        
+    for row in tags:
+        mysql_engine.execute("INSERT INTO {0}_tag VALUES ('{1}', '{2}')".format(project, row[0], row[1]))
+
+    for row in malware:
+        mysql_engine.execute("INSERT INTO {0}_malware VALUES ('{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}' )".format(project, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]))
+        
+    #rebuild association table with foreign keys
+    for row in association:
+        if row[0] == None:
+            tag_id = "Null"
+        else:
+            tag_id = "(SELECT id from default_tag WHERE id='{0}')".format(row[0])
+        if row[1] == None:
+            note_id = "Null"
+        else:
+            note_id = "(SELECT id from default_note WHERE id='{0}')".format(row[1])
+        if row[2] == None:
+            malware_id = "Null"
+        else:
+            malware_id = "(SELECT id from default_malware WHERE id='{0}')".format(row[2])
+            
+        mysql_engine.execute("INSERT INTO {0}_association VALUES ({1}, {2}, {3})".format(project, tag_id, note_id, malware_id))
+
 
 def migrate_to_mysql():
     print "Backing up Sqlite DB"
@@ -82,97 +122,76 @@ def migrate_to_mysql():
     try:
         shutil.copy('viper.db', 'viper.db.bak')
     except:
-        cont = raw_input("Failed to Backup. Do you wish to continue? Y/n")
+        cont = raw_input("Failed to Backup. Do you wish to continue? Y/n\n")
         if cont != 'Y':
             print "User stopped Migration"
             return
             
+
     sql_host = raw_input("MySQL Host Name: ")
     sql_user = raw_input("MySQL User Name: ")
-    sql_pass = getpass.getpass()
+    sql_pass = getpass.getpass("MySQl Password: ")
     sql_name = raw_input("MySQL DB Name: ")
-    
-    
-    project = 'default'
-    db_path = 'viper.db'
-    
-    print "  - Connecting to Viper Database {0}".format(db_path)
-    sqlite_engine = create_engine('sqlite:///{0}'.format(db_path))
-    
 
-    malware = sqlite_engine.execute('SELECT * FROM malware').fetchall()
-    association = sqlite_engine.execute('SELECT * FROM association').fetchall()
-    notes = sqlite_engine.execute('SELECT * FROM note').fetchall()
-    tags = sqlite_engine.execute('SELECT * FROM tag').fetchall()
-    
-    
     print " - Connecting to MySQl Database"
     mysql_conn_string = 'mysql://{0}:{1}@{2}/{3}'.format(sql_user, sql_pass, sql_host, sql_name)
     mysql_engine = create_engine(mysql_conn_string)
     
-    for row in malware:
-        mysql_engine.execute("INSERT INTO {}_malware ('name', 'size', 'type', 'mime', 'md5', 'crc32', 'sha1', 'sha256', 'sha512', 'ssdeep', 'created_at') VALUES ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} )".format(project, row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]))
+    print "Migrating Default Database"
+    #create_sql('default', 'viper.db', mysql_engine)
+
+    print "Default Migrated"
+
+    print "Migrating Projects"
+    try:
+        for project in os.listdir('projects'):
+            try:
+                print "Project - {0}".format(project)
+                create_sql(project, os.path.join('projects', project, 'viper.db'), mysql_engine)
+                print "{0} Migrated".format(project)
+            except:
+                print "Unable to migrate project ", project
+    except Exception as e:
+        print "Unable to open projects"
         
-    for row in association:
-        mysql_engine.execute("INSERT INTO {}_association ('tag_id', 'note_id', 'malware_id') VALUES ({}, {}, {})".format(project, row[0], row[1], row[2]))
-        
-    for row in notes:
-        mysql_engine.execute("INSERT INTO {}_note ('title', 'body') VALUES ({0}, {1})".format(project, row[1], row[2]))
-        
-    for row in tags:
-        mysql_engine.execute("INSERT INTO {}_tag ('tag') VALUES ({0})".format(project, row[1]))
-
-    # Create new Indexes
-    new_ix_list = ['CREATE INDEX ix_{0}_malware_md5 ON {0}_malware (md5)'.format(project),
-                   'CREATE INDEX ix_{0}_malware_sha256 ON {0}_malware (sha256)'.format(project),
-                   'CREATE UNIQUE INDEX {0}_hash_index ON {0}_malware (md5, crc32, sha1, sha256, sha512)'.format(project),
-                   'CREATE UNIQUE INDEX ix_{0}_tag_tag ON {0}_tag (tag)'.format(project)
-                  ]
-    print "    - Recreating Indexes"
-    for ix in new_ix_list:
-        try:
-            result = mysql_engine.execute(ix)
-        except Exception as e:
-            print "    - ", e
-
-
-
-
-
-
-
-print "========================================================================="
-print "| This script will migrate your Viper.db sqlite file to the new format. |"
-print "| Please create a backup of this file before preceding.                 |"
-print "| This script can also migrate to MySQL.                                |"
-print "=========================================================================\n"
+    print "Migration Complete"
+    
 
 # Main
 if __name__ == "__main__":
-    parser = OptionParser(usage='usage: %prog ' )
+    parser = OptionParser(usage='usage: %prog -s|-m' )
     parser.add_option("-s", "--sqlite", action='store_true', default=False, help="Convert to New SQlite")
     parser.add_option("-m", "--mysql", action='store_true', default=False, help="Migrate to MySQL")
-    parser.add_option("-c", "--confirm", action='store_true', default=False, help="Confirm Changes")
     
     (options, args) = parser.parse_args()
-
-    if not options.confirm:
-        print "You need to confirm changes with -c"
+    
+    if not options.sqlite and not options.mysql:
+        print ""
+        print "========================================================================="
+        print "| This script will migrate your Viper.db sqlite file to the new format. |"
+        print "| It will try to create a backup of this file before preceding.         |"
+        print "| This script can also migrate your DB to MySQL.                        |"
+        print "=========================================================================\n"
+        parser.print_help()
         sys.exit()
-        
+    
     if options.sqlite:
         print "Migrating to New SQlite format"
         migrate_to_sqlite()
         
-    if options.mysql:
+    elif options.mysql:
         print "Migrating to MySQL"
         print "To Migrate to MySQL follow these steps."
         print "1. Configure the Viper Config File to match your SQL Setup."
-        print "2. Run Viper in order to create the initial tables"
-        print "3. close viper and re run this command."
-        confirm = raw_input("Have you completed the steps? Y/n")
+        print "2. Run Viper in order to create the initial tables. VIPER WILL SAY IT HAS 0 SAMPLES"
+        print "You need to open each of your projects as well. "
+        print "3. close viper and re run this script."
+        confirm = raw_input("Have you completed the steps? Y/n\n")
         if confirm in ['Y', 'y']:
             migrate_to_mysql()
+            
+    else:
+        print "You need to pick an option either -s or -m"
 
 
 
