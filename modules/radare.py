005e640065141b9d9104884b43458d8ad05ea908
Raphaël Vinot <raphael@vinot.info>
2015-10-23 15:31:30 +0200

    Open session on file passed as argument is no sessions are open.
    Fix #2

--- modules/radare.py
+++ modules/radare.py
@@ -17,11 +17,13 @@ class Radare(Module):
     def __init__(self):
         super(Radare, self).__init__()
         self.parser.add_argument('command', nargs='*', help='Run a radare2 command on the current file')
-        self.server = ''
 
     def open_radare(self):
-        command_line = 'r2 {} {}'.format(self.server, __sessions__.current.file.path)
-        os.system(command_line)
+        command_line = 'r2 {}'.format(__sessions__.current.file.path)
+        try:
+            os.system(command_line)
+        except:
+            self.log('error', "Unable to start Radare2")
 
     def command(self, command):
         r = r2pipe.open(__sessions__.current.file.path)
@@ -30,15 +32,17 @@ class Radare(Module):
     def run(self):
         super(Radare, self).run()
 
-        if not __sessions__.is_set():
-            self.log('error', "No session opened")
-            return
-
         r2command = ' '.join(self.args.command)
-        if not r2command:
-            try:
+        if not __sessions__.is_set():
+            if os.path.isfile(r2command):
+                __sessions__.new(r2command)
                 self.open_radare()
-            except:
-                self.log('error', "Unable to start Radare2")
+                return
+            else:
+                self.log('error', "No session opened")
+                return
+
+        if not r2command:
+            self.open_radare()
         else:
             self.command(r2command)
