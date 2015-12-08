f779038c667479cc39dc7cbd924aec5146fbb831
Raphaël Vinot <raphael@vinot.info>
2015-12-08 18:19:06 +0100

    [PDNS] Add MISP support via session

--- modules/pdns.py
+++ modules/pdns.py
@@ -5,6 +5,7 @@
 from pypdns import PyPDNS
 
 from viper.common.abstracts import Module
+from viper.core.session import __sessions__
 from viper.core.config import Config
 
 cfg = Config()
@@ -20,7 +21,40 @@ class Pdns(Module):
         self.parser.add_argument("--url", help='URL of the Passive DNS server')
         self.parser.add_argument("-u", "--user", help='Username on the PDNS instance')
         self.parser.add_argument("-p", "--password", help='Password on the PDNS instance')
-        self.parser.add_argument("query", help='Domain or IP address to query')
+        self.parser.add_argument("query", nargs='?', default=None, help='Domain or IP address to query')
+
+        self.parser.add_argument("-v", "--verbose", action='store_true', help="Turn on verbose mode.")
+        self.parser.add_argument('-m', '--misp', default=None, choices=['ips', 'domains'],
+                                 help='Searches for the ips or domains from the current MISP event')
+
+    def misp(self, option, verbose=False):
+        if not __sessions__.is_attached_misp():
+            return
+
+        if option == 'ips':
+            ips = __sessions__.current.misp_event.get_all_ips()
+            for ip in ips:
+                self.query(ip, verbose)
+        elif option == 'domains':
+            domains = __sessions__.current.misp_event.get_all_domains()
+            for d in domains:
+                self.query(d, verbose)
+
+    def query(self, q, verbose):
+        try:
+            data = self.pdns.query(q)
+        except Exception as e:
+            self.log('error', e)
+            return
+        if not data:
+            self.log('error', 'Unable to find {}.'.format(self.args.query))
+            return
+        if not verbose:
+            data = data[-10:]
+        res_rows = [(d['time_first'].strftime("%Y/%m/%d %H:%M"), d['time_last'].strftime("%Y/%m/%d %H:%M"),
+                     d['count'], d['rrname'], d['rdata']) for d in data]
+        self.log('success', 'Passive DNS for {}:'.format(q))
+        self.log('table', dict(header=['First valid', 'Last valid', 'Times seen', 'RR Name', 'Content'], rows=res_rows))
 
     def run(self):
         super(Pdns, self).run()
@@ -43,16 +77,9 @@ class Pdns(Module):
         else:
             password = cfg.pdns.pdns_pass
 
-        pdns = PyPDNS(url.rstrip('/'), (user, password))
-        try:
-            data = pdns.query(self.args.query)
-        except Exception as e:
-            self.log('error', e)
-            return
-        if not data:
-            self.log('error', 'Unable to find {}.'.format(self.args.query))
-            return
-        for d in data:
-            self.log('success', "{} -> {} ({} requests)\n\t RR Name: {} - Data: {}".format(
-                d['time_first'].strftime("%Y/%m/%d %H:%M"), d['time_last'].strftime("%Y/%m/%d %H:%M"),
-                d['count'], d['rrname'], d['rdata']))
+        self.pdns = PyPDNS(url.rstrip('/'), (user, password))
+
+        if self.args.misp:
+            self.misp(self.args.misp, self.args.verbose)
+        else:
+            self.query(self.args.query, self.args.verbose)
