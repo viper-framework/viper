0f1936c67e74e205cef4880deff6d7312b4e4c7a
Nex <nex@nex.sx>
2016-01-04 10:31:33 +0100

    Cleaned code a bit

--- viper/common/autorun.py
+++ viper/common/autorun.py
@@ -13,43 +13,53 @@ from viper.core.storage import get_sample_path
 cfg = Config()
 
 # Read a list of command lines from the config file and execute them each time a sample is STORED.
-
 def parse(data):
     root = ''
     args = []
     words = data.split()
     root = words[0]
+
     if len(words) > 1:
         args = words[1:]
+
     return (root, args)
 
 def autorun_module(file_hash):
     if not file_hash:
         return
+
     # We need an open session
     if not __sessions__.is_set():
         # Open session
         __sessions__.new(get_sample_path(file_hash))
+
     for cmd_line in cfg.autorun.commands.split(','):
         split_commands = cmd_line.split(';')
+
         for split_command in split_commands:
             split_command = split_command.strip()
+
             if not split_command:
                 continue
+
             root, args = parse(split_command)
+
             try:
                 if root in __modules__:
+                    print_info("Running command \"{0}\"".format(split_command))
+
                     module = __modules__[root]['obj']()
                     module.set_commandline(args)
                     module.run()
-                    print_info("Running Command {0}".format(split_command))
+                    
                     if cfg.modules.store_output and __sessions__.is_set():
                         Database().add_analysis(file_hash, split_command, module.output)
+                    
                     if cfg.autorun.verbose:
                         print_output(module.output)
+
                     del(module.output[:])
                 else:
-                    print_error('{0} is not a valid command. Please check your viper.conf file.'.format(cmd_line))
+                    print_error("\"{0}\" is not a valid command. Please check your viper.conf file.".format(cmd_line))
             except:
-                print_error('Viper was unable to complete the command {0}'.format(cmd_line))
-    return 
+                print_error("Viper was unable to complete the command {0}".format(cmd_line))
