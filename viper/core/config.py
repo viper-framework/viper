dc8ebb56898e414cd80d75c2ab55595da224f6d7
Alessandro Gario <alessandro.gario@gmail.com>
2016-01-07 21:47:54 +0100

    Always create the default configuration file inside the home folder under ~/.viper.conf.
    Automatically set the 'paths.store_root' configuration field to ~/Viper
    when creating a new configuration file.

--- viper/core/config.py
+++ viper/core/config.py
@@ -12,11 +12,12 @@ from viper.common.objects import Dictionary
 class Config:
     
     def __init__(self, file_name="viper", cfg=None):
-    
+        default_configuration_path = os.path.expanduser('~/.viper.conf')
+
         config = ConfigParser.ConfigParser()
-        
+
         if cfg == None:
-            cfg = os.path.expanduser('~/.viper.conf')
+            cfg = default_configuration_path
 
         test = config.read(cfg)
 
@@ -25,8 +26,18 @@ class Config:
             print_error("Could not find a valid configuration file. Did you copy viper.conf.sample to ~/.viper.conf?")
             print_info("Trying to create config for you")
             try:
-                shutil.copy('viper.conf.sample', 'viper.conf')
-                config.read('viper.conf')
+                viper_installation_directory = os.path.dirname(os.path.realpath(__file__))
+                sample_configuration_path = os.path.join(viper_installation_directory, '../../viper.conf.sample')
+
+                shutil.copy(sample_configuration_path, default_configuration_path)
+
+                config.read(default_configuration_path)
+                default_repository_path = os.path.expanduser('~/Viper')
+
+                print_info("Setting \"" + default_repository_path + "\" as your default repository...")
+                config.set('paths', 'store_path', default_repository_path)
+                config.write(open(default_configuration_path, "w"))
+
                 print_info("Starting Viper")
             except:
                 print_error("Failed to Create config file, Exiting")
