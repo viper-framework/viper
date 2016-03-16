1e2deffab369ad164d4b789b42cca641e78850e5
magrazia <magrazia@sourcefire.com>
2016-03-16 02:59:08 -0700

    Added entrypoint, machine adn type command and cluster and scan methods

--- viper/modules/elf.py
+++ viper/modules/elf.py
@@ -1,6 +1,8 @@
 # This file is part of Viper - https://github.com/viper-framework/viper
 # See the file 'LICENSE' for copying permission.
 
+import os
+
 try:
     from elftools.elf.elffile import ELFFile
     from elftools.elf.sections import SymbolTableSection
@@ -12,8 +14,11 @@ try:
 except ImportError:
     HAVE_ELFTOOLS = False
 
+from viper.common.out import bold
 from viper.common.abstracts import Module
 from viper.core.session import __sessions__
+from viper.core.database import Database
+from viper.core.storage import get_sample_path
 
 
 # Have a look at scripts/readelf.py - pyelftools
@@ -24,11 +29,28 @@ class ELF(Module):
 
     def __init__(self):
         super(ELF, self).__init__()
-        self.parser.add_argument('--sections', action='store_true', help='List ELF sections')
-        self.parser.add_argument('--segments', action='store_true', help='List ELF segments')
-        self.parser.add_argument('--symbols', action='store_true', help='List ELF symbols')
-        self.parser.add_argument('--interpreter', action='store_true', help='Get the program interpreter')
-        self.parser.add_argument('--dynamic', action='store_true', help='Show the dynamic section')
+        subparsers = self.parser.add_subparsers(dest='subname')
+        
+        subparsers.add_parser('sections', help='List ELF sections')
+        subparsers.add_parser('segments', help='List ELF segments')
+        subparsers.add_parser('symbols', help='List ELF symbols')
+        subparsers.add_parser('interpreter', help='Get the program interpreter')
+        subparsers.add_parser('dynamic', help='Show the dynamic section')
+        
+        parser_ep = subparsers.add_parser('entrypoint', help='Show the ELF entry point')
+        parser_ep.add_argument('-a', '--all', action='store_true', help='Print the entry point of all files in the project')
+        parser_ep.add_argument('-c', '--cluster', action='store_true', help='Cluster all files in the project')
+        parser_ep.add_argument('-s', '--scan', action='store_true', help='Scan repository for matching samples')
+
+        parser_machine = subparsers.add_parser('machine', help='Show the ELF machine')
+        parser_machine.add_argument('-a', '--all', action='store_true', help='Print the machine for all files in the project')
+        parser_machine.add_argument('-c', '--cluster', action='store_true', help='Cluster all files in the project')
+        parser_machine.add_argument('-s', '--scan', action='store_true', help='Scan repository for matching samples')
+
+        parser_type = subparsers.add_parser('type', help='Show the ELF type')
+        parser_type.add_argument('-a', '--all', action='store_true', help='Print the type of all files in the project')
+        parser_type.add_argument('-c', '--cluster', action='store_true', help='Cluster all files in the project')
+        parser_type.add_argument('-s', '--scan', action='store_true', help='Scan repository for matching samples')
         self.elf = None
 
     def __check_session(self):
@@ -128,6 +150,288 @@ class ELF(Module):
                     continue
                 self.log('info', tag.needed)
 
+    def entrypoint(self):
+        if not self.__check_session():
+            return
+
+        ep = self.elf.header.e_entry
+        self.log('info', "Entry point: {0:#x}".format(ep))
+        
+        if self.args.scan and self.args.cluster:
+            self.log('error', "You selected two exclusive options, pick one")
+            return
+
+        if self.args.all:
+            db = Database()
+            samples = db.find(key='all')
+
+            rows = []
+            for sample in samples:
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try:
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_ep = elfo.header.e_entry
+                except:
+                    continue
+
+                rows.append([sample.md5, sample.name, hex(cur_ep)])
+
+            self.log('table', dict(header=['MD5', 'Name', 'Entry point'], rows=rows))
+
+            return
+
+
+        if self.args.cluster:
+            db = Database()
+            samples = db.find(key='all')
+
+            cluster = {}
+            for sample in samples:
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try: 
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_ep = hex(elfo.header.e_entry)
+                except Exception as e:
+                    self.log('error', "Error {0} for sample {1}".format(e, sample.sha256))
+                    continue
+
+                if cur_ep not in cluster:
+                    cluster[cur_ep] = []
+
+                cluster[cur_ep].append([sample.md5, sample.name])
+
+            for cluster_name, cluster_members in cluster.items():
+                # Skipping clusters with only one entry.
+                if len(cluster_members) == 1:
+                    continue
+
+                self.log('info', "ELF entry point cluster {0} with {1} elements".format(bold(cluster_name), len(cluster_members)))
+
+                self.log('table', dict(header=['MD5', 'Name'], rows=cluster_members))
+
+        if self.args.scan:
+            db = Database()
+            samples = db.find(key='all')
+
+            rows = []
+            for sample in samples:
+                if sample.sha256 == __sessions__.current.file.sha256:
+                    continue
+
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try:
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_ep = elfo.header.e_entry
+                except:
+                    continue
+
+                if ep == cur_ep:
+                    rows.append([sample.md5, sample.name])
+
+            if len(rows) > 0:
+                self.log('info', "Following are samples with Entry point {0}".format(bold(ep)))
+                self.log('table', dict(header=['MD5', 'Name'], rows=rows))
+
+    def machine(self):
+        if not self.__check_session():
+            return
+
+        machine = self.elf.header.e_machine
+        self.log('info', "Machine: {0:s}".format(machine))
+
+        if self.args.scan and self.args.cluster:
+            self.log('error', "You selected two exclusive options, pick one")
+            return
+
+        if self.args.all:
+            db = Database()
+            samples = db.find(key='all')
+
+            rows = []
+            for sample in samples:
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try:
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_machine = elfo.header.e_machine
+                except:
+                    continue
+
+                rows.append([sample.md5, sample.name, cur_machine])
+
+            self.log('table', dict(header=['MD5', 'Name', 'e_machine'], rows=rows))
+
+            return
+
+
+        if self.args.cluster:
+            db = Database()
+            samples = db.find(key='all')
+
+            cluster = {}
+            for sample in samples:
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try: 
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_machine = elfo.header.e_machine
+                except Exception as e:
+                    self.log('error', "Error {0} for sample {1}".format(e, sample.sha256))
+                    continue
+
+                if cur_machine not in cluster:
+                    cluster[cur_machine] = []
+
+                cluster[cur_machine].append([sample.md5, sample.name])
+
+            for cluster_name, cluster_members in cluster.items():
+                # Skipping clusters with only one entry.
+                if len(cluster_members) == 1:
+                    continue
+
+                self.log('info', "ELF e_machine cluster {0} with {1} elements".format(bold(cluster_name), len(cluster_members)))
+
+                self.log('table', dict(header=['MD5', 'Name'], rows=cluster_members))
+
+        if self.args.scan:
+            db = Database()
+            samples = db.find(key='all')
+
+            rows = []
+            for sample in samples:
+                if sample.sha256 == __sessions__.current.file.sha256:
+                    continue
+
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try:
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_machine = elfo.header.e_machine
+                except:
+                    continue
+
+                if machine == cur_machine:
+                    rows.append([sample.md5, sample.name])
+
+            if len(rows) > 0:
+                self.log('info', "Following are samples with Entry point {0}".format(bold(ep)))
+                self.log('table', dict(header=['MD5', 'Name'], rows=rows))
+
+    def elftype(self):
+        if not self.__check_session():
+            return
+
+        e_type = self.elf.header.e_type
+        self.log('info', "ELF type: {0:s}".format(e_type))
+
+        if self.args.scan and self.args.cluster:
+            self.log('error', "You selected two exclusive options, pick one")
+            return
+
+        if self.args.all:
+            db = Database()
+            samples = db.find(key='all')
+
+            rows = []
+            for sample in samples:
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try:
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_e_type = elfo.header.e_type
+                except:
+                    continue
+
+                rows.append([sample.md5, sample.name, cur_e_type])
+
+            self.log('table', dict(header=['MD5', 'Name', 'e_type'], rows=rows))
+
+            return
+
+
+        if self.args.cluster:
+            db = Database()
+            samples = db.find(key='all')
+
+            cluster = {}
+            for sample in samples:
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try: 
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_e_type = elfo.header.e_type
+                except Exception as e:
+                    self.log('error', "Error {0} for sample {1}".format(e, sample.sha256))
+                    continue
+
+                if cur_e_type not in cluster:
+                    cluster[cur_e_type] = []
+
+                cluster[cur_e_type].append([sample.md5, sample.name])
+
+            for cluster_name, cluster_members in cluster.items():
+                # Skipping clusters with only one entry.
+                if len(cluster_members) == 1:
+                    continue
+
+                self.log('info', "ELF e_type cluster {0} with {1} elements".format(bold(cluster_name), len(cluster_members)))
+
+                self.log('table', dict(header=['MD5', 'Name'], rows=cluster_members))
+
+        if self.args.scan:
+            db = Database()
+            samples = db.find(key='all')
+
+            rows = []
+            for sample in samples:
+                if sample.sha256 == __sessions__.current.file.sha256:
+                    continue
+
+                sample_path = get_sample_path(sample.sha256)
+                if not os.path.exists(sample_path):
+                    continue
+
+                try:
+                    fd = open(sample_path, 'rb')
+                    elfo = ELFFile(fd)
+                    cur_e_type = elfo.header.e_machine
+                except:
+                    continue
+
+                if machine == cur_machine:
+                    rows.append([sample.md5, sample.name])
+
+            if len(rows) > 0:
+                self.log('info', "Following are samples with ELF type {0}".format(bold(e_type)))
+                self.log('table', dict(header=['MD5', 'Name'], rows=rows))
+
     def run(self):
         super(ELF, self).run()
         if self.args is None:
@@ -137,16 +441,22 @@ class ELF(Module):
             self.log('error', "Missing dependency, install pyelftools (`pip install pyelftools`)")
             return
 
-        if self.args.sections:
+        if self.args.subname == "sections":
             self.sections()
-        elif self.args.segments:
+        elif self.args.subname == "segments":
             self.segments()
-        elif self.args.symbols:
+        elif self.args.subname == "symbols":
             self.symbols()
-        elif self.args.interpreter:
+        elif self.args.subname == "interpreter":
             self.interpreter()
-        elif self.args.dynamic:
+        elif self.args.subname == "dynamic":
             self.dynamic()
+        elif self.args.subname == "entrypoint":
+            self.entrypoint()
+        elif self.args.subname == "machine":
+            self.machine()
+        elif self.args.subname == "type":
+            self.elftype()
         else:
             self.log('error', 'At least one of the parameters is required')
             self.usage()
