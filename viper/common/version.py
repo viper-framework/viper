c0b2e70b699580f817a1626e9dcfb91aea766767
Robert Habermann <mail@rhab.de>
2017-03-26 14:55:45 +0200

    fix typo

--- viper/common/version.py
+++ viper/common/version.py
@@ -5,7 +5,7 @@
 # Store the version here so:
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
-# 3) we can import it into your module module
+# 3) we can import it into modules
 
 __version_info__ = ('1', '3-dev')
 __version__ = '.'.join(__version_info__)
