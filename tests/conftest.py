aaccd2c89ebcb937ff88ac860d89e283bf821b89
Robert Habermann <mail@rhab.de>
2017-04-15 15:21:48 +0200

    add tests for APK module
    Create initial tests for APK module. Samples .apk file is just a simple
    hello world app that I wrote. The dump option fails and I assume this is
    a Python3 issue. Needs to be checked further.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,24 @@
 # This file is part of Viper - https://github.com/viper-framework/viper
 # See the file 'LICENSE' for copying permission.
 
+import pytest
+import tempfile
 import os
+import shutil
 
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'viper-test-files/test_files',)
+
+
+@pytest.fixture()
+def cleandir():
+    newpath = tempfile.mkdtemp(prefix="viper_tests_tmp_")
+    os.chdir(newpath)
+    yield
+    shutil.rmtree(newpath)
+
+
+@pytest.fixture()
+def cleandir_keep():
+    newpath = tempfile.mkdtemp(prefix="viper_tests_tmp_")
+    os.chdir(newpath)
+    yield
