8f5b439d38caae853e45c02cacc91c6ad18cac05
Raphaël Vinot <raphael@vinot.info>
2017-05-03 13:11:04 +0200

    Move path_split_all to viper-update

--- viper/common/utils.py
+++ viper/common/utils.py
@@ -2,7 +2,6 @@
 # This file is part of Viper - https://github.com/viper-framework/viper
 # See the file 'LICENSE' for copying permission.
 
-import os
 import string
 import hashlib
 import binascii
@@ -12,24 +11,6 @@ try:
     import magic
 except ImportError:
     pass
-
-
-# Taken from the Python Cookbook.
-def path_split_all(path):
-    allparts = []
-    while 1:
-        parts = os.path.split(path)
-        if parts[0] == path:
-            allparts.insert(0, parts[0])
-            break
-        elif parts[1] == path:
-            allparts.insert(0, parts[1])
-            break
-        else:
-            path = parts[0]
-            allparts.insert(0, parts[1])
-
-    return allparts
 
 
 # The following couple of functions are redundant.
