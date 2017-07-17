03af1798cebe8ab88403413476ed8b0dadafe04f
Raphaël Vinot <raphael@vinot.info>
2017-07-17 10:44:50 +0200

    Make flake8 happy

--- viper/modules/scrap/__init__.py
+++ viper/modules/scrap/__init__.py
@@ -1,2 +1,2 @@
-from .middleware import CustomSplashRequestDepthMiddleware
-from .crawler import CustomCrawler
+from .middleware import CustomSplashRequestDepthMiddleware  # flake8: noqa
+from .crawler import CustomCrawler  # flake8: noqa
