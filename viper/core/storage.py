6cce8a6cce8ae0e3052d0e76d25f6bfc62c67041
Robert Habermann <mail@rhab.de>
2018-02-04 22:50:13 +0100

    change the way yara rules and peid files are handled

--- viper/core/storage.py
+++ viper/core/storage.py
@@ -3,9 +3,11 @@
 # See the file 'LICENSE' for copying permission.
 
 import os
+import shutil
 
-from viper.common.out import print_warning, print_error
+from viper.common.out import print_warning, print_error, print_info
 from viper.core.project import __project__
+from viper.common.constants import VIPER_ROOT, DIST_DIR_YARA_RULES, DIST_DIR_PEID
 
 
 def store_sample(file_object):
@@ -55,3 +57,48 @@ def get_sample_path(sha256):
         return None
 
     return path
+
+
+def check_and_deploy_yara_rules():
+    """Yara: check whether rule path exist - if not copy default set of rules to directory"""
+    yara_rules_path = os.path.join(__project__.base_path, "yara")
+    if os.path.exists(yara_rules_path):
+        print_info("Using Yara rules from directory: {}".format(yara_rules_path))
+    else:
+        # Prio 1: rules if Viper was installed with pip
+        yara_path_setup_utils = os.path.join(VIPER_ROOT, DIST_DIR_YARA_RULES)
+
+        # Prio 2: rules if Viper was checkout from repo
+        yara_path_repo = os.path.join(VIPER_ROOT, "data", "yara")
+
+        if os.path.exists(yara_path_setup_utils):
+            print_warning("Yara rule directory not found - copying default "
+                          "rules ({}) to: {}".format(yara_path_setup_utils, yara_rules_path))
+
+            shutil.copytree(yara_path_setup_utils, yara_rules_path)
+        elif os.path.exists(yara_path_repo):
+            print_warning("Yara rule directory not found - copying default "
+                          "rules ({}) to: {}".format(yara_path_repo, yara_rules_path))
+            shutil.copytree(yara_path_repo, yara_rules_path)
+        else:
+            print_error("No default Yara rules found")
+
+
+def check_and_deploy_peid():
+    """PEID: check whether PEID dir exist - if not copy default to directory"""
+    peid_path = os.path.join(__project__.base_path, "peid")
+    if os.path.exists(peid_path):
+        print_info("Using PEID info from directory: {}".format(peid_path))
+    else:
+        # Prio 1: peid info if Viper was installed with pip
+        peid_path_setup_utils = os.path.join(VIPER_ROOT, DIST_DIR_PEID)
+
+        # Prio 2: peid info if Viper was checkout from repo
+        peid_path_repo = os.path.join(VIPER_ROOT, "data", "peid")
+
+        if os.path.exists(peid_path_setup_utils):
+            shutil.copytree(peid_path_setup_utils, peid_path)
+        elif os.path.exists(peid_path_repo):
+            shutil.copytree(peid_path_repo, peid_path)
+        else:
+            pass
