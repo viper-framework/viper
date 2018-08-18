eccdfc8674e2afe6a4e1b1487091b445bb40f898
Robert Habermann <mail@rhab.de>
2018-08-18 11:04:54 +0000

    fix tests

--- viper/web/wsgi.py
+++ viper/web/wsgi.py
@@ -6,9 +6,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise import WhiteNoise
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
