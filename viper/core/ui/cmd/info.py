ca8004fab8f3986cf6a47642448f56159af0f75b
Nex <nex@nex.sx>
2018-08-22 02:11:54 +0200

    Error message on no session

--- viper/core/ui/cmd/info.py
+++ viper/core/ui/cmd/info.py
@@ -21,23 +21,26 @@ class Info(Command):
         except SystemExit:
             return
 
-        if __sessions__.is_set():
-            self.log('table', dict(
-                header=['Key', 'Value'],
-                rows=[
-                    ['Name', __sessions__.current.file.name],
-                    ['Tags', __sessions__.current.file.tags],
-                    ['Path', __sessions__.current.file.path],
-                    ['Size', __sessions__.current.file.size],
-                    ['Type', __sessions__.current.file.type],
-                    ['Mime', __sessions__.current.file.mime],
-                    ['MD5', __sessions__.current.file.md5],
-                    ['SHA1', __sessions__.current.file.sha1],
-                    ['SHA256', __sessions__.current.file.sha256],
-                    ['SHA512', __sessions__.current.file.sha512],
-                    ['SSdeep', __sessions__.current.file.ssdeep],
-                    ['CRC32', __sessions__.current.file.crc32],
-                    ['Parent', __sessions__.current.file.parent],
-                    ['Children', __sessions__.current.file.children]
-                ]
-            ))
+        if not __sessions__.is_set():
+            self.log('error', "No open session. This command expects a file to be open.")
+            return
+
+        self.log('table', dict(
+            header=['Key', 'Value'],
+            rows=[
+                ['Name', __sessions__.current.file.name],
+                ['Tags', __sessions__.current.file.tags],
+                ['Path', __sessions__.current.file.path],
+                ['Size', __sessions__.current.file.size],
+                ['Type', __sessions__.current.file.type],
+                ['Mime', __sessions__.current.file.mime],
+                ['MD5', __sessions__.current.file.md5],
+                ['SHA1', __sessions__.current.file.sha1],
+                ['SHA256', __sessions__.current.file.sha256],
+                ['SHA512', __sessions__.current.file.sha512],
+                ['SSdeep', __sessions__.current.file.ssdeep],
+                ['CRC32', __sessions__.current.file.crc32],
+                ['Parent', __sessions__.current.file.parent],
+                ['Children', __sessions__.current.file.children]
+            ]
+        ))
