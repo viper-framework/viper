406c68c091f99f7a34b0895d08ed0e8622b2e6ed
Raphaël Vinot <raphael@vinot.info>
2019-08-05 15:01:08 +0200

    fix: Wrong call to log.

--- viper/modules/rtf.py
+++ viper/modules/rtf.py
@@ -69,7 +69,7 @@ class Rtf(Module):
                     _, temp_ext = os.path.splitext(rtfobj.temp_path)
                     self.log('debug', 'Temp path extension: {ext}'.format(ext=temp_ext))
                     _, file_ext = os.path.splitext(rtfobj.filename)
-                    log.debug('File extension: %r' % file_ext)
+                    self.log.debug('File extension: %r' % file_ext)
 
                     if temp_ext != file_ext:
                         obj_col.append("MODIFIED FILE EXTENSION")
@@ -98,7 +98,7 @@ class Rtf(Module):
         self.log('table', dict(rows=table, header=h))
 
     def list(self):
-        self.parse_rtf(__sessions__.current.file.name, __sessions__.current.file.data) 
+        self.parse_rtf(__sessions__.current.file.name, __sessions__.current.file.data)
 
     def save_ole_objects(self, data, save_object, filename):
         '''
