d629da3afa925b9e481668257f0a434710f3dc52
Nex <nex@nex.sx>
2019-11-11 20:10:14 +0100

    Added command to update modules

--- viper/core/ui/cmd/update-modules.py
+++ viper/core/ui/cmd/update-modules.py
@@ -2,9 +2,11 @@
 # This file is part of Viper - https://github.com/viper-framework/viper
 # See the file 'LICENSE' for copying permission.
 
-from viper.common.abstracts import Command
-from viper.core.plugins import load_commands, __modules__
+import os
+import sys
+import subprocess
 
+from viper.common.abstracts import Command
 
 class UpdateModules(Command):
     """
@@ -16,3 +18,24 @@ class UpdateModules(Command):
 
     def run(self, *args):
         self.log("info", "Updating modules...")
+
+        dot_viper = os.path.join(os.path.expanduser("~"), ".viper")
+        # Clone the repository.
+        p = subprocess.Popen(["git", "clone", "git@github.com:viper-framework/viper-modules.git",
+            "modules"], cwd=dot_viper)
+        p.wait()
+        dot_viper_modules = os.path.join(dot_viper, "modules")
+        # Initialize submodules.
+        p = subprocess.Popen(["git", "submodule", "init"], cwd=dot_viper_modules)
+        p.wait()
+        # Update submodules.
+        p = subprocess.Popen(["git", "submodule", "update"], cwd=dot_viper_modules)
+        p.wait()
+        # Install dependencies.
+        p = subprocess.Popen(["pip3", "install", "-r", "requirements.txt"], cwd=dot_viper_modules)
+        p.wait()
+
+        # TODO: this is terrible. We need to find a way to move __modules__
+        # to  proper place that can be reloaded.
+        self.log("info", "Modules updated, please relaunch `viper`.")
+        sys.exit()
