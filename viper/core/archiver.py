4c39344ae72a5ecf8b9e712378d6a949dfc55bd6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 23:59:25 +0000

    Adding tarfile member sanitization to extractall()

--- viper/core/archiver.py
+++ viper/core/archiver.py
@@ -364,7 +364,26 @@ class TarExtractor(Extractor):
 
         try:
             with tarfile.open(self.input_path, 'r:*') as tarf:
-                tarf.extractall(self.output_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tarf, self.output_path)
 
         except Exception as err:
             log.error("Extract ({}) failed. Error: {}".format(self.title, err))
